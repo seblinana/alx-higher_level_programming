58ba89f1fd27bd92800049e83b287166d013ed06
Seblewongel Petros <88587773+seblinana@users.noreply.github.com>
2021-11-17 01:49:40 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(*["%c" % a for a in range(ord('a'),ord('z')+1) if a not in (ord('q'),ord('e'))],sep='',end='')
+for alpha_letters in range(ord('a'), ord('z')+1):
+  if ord(alpha_letters) ==101  or ord(alpha_letters) == 113:
+    continue
+    print("{:c}".format(alpha_letters), end="")
